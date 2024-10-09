b9af6e1a945196c766cf9c31dc40f0805e701b98
Chandradeep-Pra <chandradeepprasad1998@gmail.com>
2024-10-10 05:17:06 +0530

    Fix - Duplicate navbar in desktop view

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Header />
       <section className="min-h-screen flex-1 flex-col px-6 pb-6 pt-24 max-md:pb-14 sm:px-14">
         <div className="mx-auto w-full max-w-5xl">{children}</div>
-        <div className="mt-6 border-2 border-amber-400 px-6 w-[90%] py-1 ml-2 rounded-full"><Navbar /></div>
+        <div className="mt-6 border-2 border-amber-400 px-6 w-[90%] py-1 ml-2 rounded-full sm:hidden"><Navbar /></div>
       </section>
       {/* <div className="sm:hidden px-6 border-2  absolute bottom-[100px] left-16 py-2 rounded-full">
         
