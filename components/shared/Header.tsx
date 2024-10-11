7509d5530f8ba8b80554aa0e25d37222ea261304
Chandradeep-Pra <chandradeepprasad1998@gmail.com>
2024-10-12 03:07:07 +0530

    fix -landing nav

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,25 +1,28 @@
-import Link from 'next/link'
-import React from 'react'
-import { BsWhatsapp } from 'react-icons/bs'
-import Navbar from './navbar/Navbar'
+import Link from "next/link";
+import React from "react";
+import { BsWhatsapp } from "react-icons/bs";
+import Navbar from "./navbar/Navbar";
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center'>
+    <div className="flex justify-between items-center">
+      <Link href="/">
         <h3 className="text-lg  gap-1 flex flex-col text-lime-300">
-        Chandradeep
-        <span className="text-2xl -mt-2 font-extralight text-white">
-          Prasad
-        </span>
-      </h3>
-    <div className='mr-10 max-lg:hidden'>
+          Chandradeep
+          <span className="text-2xl -mt-2 font-extralight text-white">
+            Prasad
+          </span>
+        </h3>
+      </Link>
+
+      <div className="mr-10 max-lg:hidden">
         <Navbar />
-    </div>
+      </div>
       <Link href="https://wa.me/+919635901369">
         <BsWhatsapp className="size-8 text-green-500" />
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
